f6a9ba30f7ca5f3ca7324f91fa6ee5ac92129bda
Lashkin_A <antonlaskin38@gmail.com>
2022-11-22 22:19:25 +0300

    Addede dataGridView

--- BigProject/Form1.cs
+++ BigProject/Form1.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BigProject.Classes;
 
 namespace BigProject
 {
     public partial class Form1 : Form
     {
+        private List<DataCacheStorage> A = new List<DataCacheStorage>();
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +22,24 @@ namespace BigProject
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataCacheStorage data = new DataCacheStorage("Name");
+            ReaderCSV R = new ReaderCSV();
+            R.reader("E:ForExample.csv", data);
+            A.Add(data);
+            foreach (var colums in data.DataColumn) { dataGridView1.DataSource = colums; }
+            dataGridView1.DataSource = data.DataColumn["1"];
+            // dataGridView1.Columns.Add(data.DataColumn[0], "Header column - 1");
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
 
         }
     }
